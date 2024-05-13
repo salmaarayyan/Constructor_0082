49802dc824f374b1b7d21a8db64e2d8c81439281
salmaarayyan <salmaarayyan@gmail.com>
2024-05-13 15:36:02 +0700

    membuat class mahasiswa

--- construktor.cpp
+++ construktor.cpp
@@ -2,3 +2,28 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa
+{
+    public:
+        int nim;
+        string nama;
+
+        Mahasiswa()
+        {
+            nim = 0;
+            nama = "";
+        };
+        Mahasiswa(int iNim)
+        {
+            nim = iNim;
+        };
+
+Mahasiswa(string);
+Mahasiswa(int iNim, string iNama);
+void cetak()
+    {
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    }
+};
